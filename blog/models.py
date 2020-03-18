1fc1597b034949fbc7f54c80b8c889419d13f036
YA <YA-androidapp@users.noreply.github.com>
2020-03-18 12:57:32 +0900

    Remove comments

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,4 @@
-# import os
-# import random
-
-# from django.conf import settings
-# from django.core.mail import send_mail
 from django.db import models
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
 from django.utils import timezone
 
 
@@ -28,26 +21,6 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-
-
-# def get_filename_ext(filepath):
-#     base_name = os.path.basename(filepath)
-#     name, ext = os.path.splitext(base_name)
-#     return name, ext
-
-
-# def upload_post_image_path(instance, filename):
-#     new_filename = random.randint(1000, 9999)
-#     name, ext = get_filename_ext(filename)
-#     final_filename = '%s%s' % (new_filename, ext)
-#     return "post_images/%s" % (final_filename)
-
-
-# def upload_post_content_image_path(instance, filename):
-#     new_filename = random.randint(10000000, 99999999)
-#     name, ext = get_filename_ext(filename)
-#     final_filename = '%s%s' % (new_filename, ext)
-#     return "post_content_images/%s" % (final_filename)
 
 
 class Post(models.Model):
@@ -99,21 +72,6 @@ class Comment(models.Model):
     def __str__(self):
         return self.text
 
-    # def send_email_notif(self):
-    #     subject = "コメント投稿"
-    #     message = "コメントが投稿されました。"
-    #     from_email = settings.DEFAULT_FROM_EMAIL
-    #     recipient_list = [settings.EMAIL_HOST_USER]
-    #     send_email = send_mail(
-    #         subject, message, from_email, recipient_list)
-    #     return send_email
-
-
-# @receiver(post_save, sender=Comment)
-# def comment_create_receiver(sender, instance, created, **kwargs):
-#     if created:
-#         instance.send_email_notif()
-
 
 class Reply(models.Model):
     comment = models.ForeignKey(
@@ -129,18 +87,3 @@ class Reply(models.Model):
 
     def __str__(self):
         return self.text
-
-    # def send_email_notif(self):
-    #     subject = "コメント返信"
-    #     message = "コメントに返信がありました。"
-    #     from_email = settings.DEFAULT_FROM_EMAIL
-    #     recipient_list = [settings.EMAIL_HOST_USER]
-    #     send_email = send_mail(
-    #         subject, message, from_email, recipient_list)
-    #     return send_email
-
-
-# @receiver(post_save, sender=Reply)
-# def comment_reply_receiver(sender, instance, created, **kwargs):
-#     if created:
-#         instance.send_email_notif()
